41a688142f4c902679e4cb6444a5971f62a531ef
durotemy <durotemy@gmail.com>
2023-04-02 23:05:00 +0100

    mobile responsiveness

--- src/pages/getstarted.jsx
+++ src/pages/getstarted.jsx
@@ -29,7 +29,7 @@ const getstarted = () => {
               Get the home of your dreams easily with our top-rated swiping app
               on IOS and Andriod
             </p>
-            <div className="flex space-x-5 mt-8">
+            <div className="flex lg:flex-row flex-col lg:justify-left lg:space-x-5 mt-8">
               <div className="bg-[black] text-[white] text-sm h-16 w-40 text-center cursor-pointer border rounded-lg flex">
                 <div className="flex mt-2 mx-auto">
                   <AiOutlineApple className="flex w-10 h-10" />
@@ -72,7 +72,7 @@ const getstarted = () => {
               height={400}
             />
           </div>
-          <div className=" w-11/12 lg:w-4/12 mt-32">
+          <div className=" w-11/12 lg:w-4/12 lg:mt-32 mt-8">
             <p className="text-[30px] text-[#05A3A3]">
               Quickly find what you need
             </p>
@@ -84,30 +84,30 @@ const getstarted = () => {
           </div>
         </div>
       </div>
-      <div className="mx-auto h-[700px] bg-gradient-to-r from-[#424A52] to-[#C1694F]">
+      <div className="mx-auto h-[800px] lg:h-[700px] bg-gradient-to-r from-[#424A52] to-[#C1694F]">
         <div className="flex lg:flex-row space-y-9 flex-col mx-auto justify-between w-10/12">
-          <div className="w-10/12 lg:w-4/12 mt-20 lg:mt-36">
+          <div className="  lg:text-auto w-10/12 lg:w-4/12 mt-20 lg:mt-36">
             <p className="text-[white] text-2xl">
               Virtually explore properties from your home
             </p>
-            <p className="text-[black] mt-8 text-1xl">
+            <p className="text-[black] mt-8 text-[19px]">
               Explore properties with a swap spac broker who will help you find
               the perfect place at the right place, or take a virtual tour from
               anywhere. Contactless meetings keep you safe till when you are
               ready to visit in person
             </p>
           </div>
-          <div className="w-11/12 justify-center  lg:w-6/12 flex lg:justify-end mt-22 lg:mt-32">
+          <div className="w-full justify-center  lg:w-6/12 flex lg:justify-end mt-22 lg:mt-32">
             <div
               style={bg}
-              className=" rounded-lg flex justify-end bg-[url('/images/phonebackground.png')] h-350 lg:h-[400px] w-9/12 bg-cover bg-center bg-no-repeat "
+              className=" rounded-lg flex justify-end bg-[url('/images/phonebackground.png')] h-[260px] lg:h-[400px] w-9/12 bg-cover bg-center bg-no-repeat "
             >
               <Image
-                className="lg:mt-60 mt-12 lg:h-[700px] h-[350px]"
+                className="lg:mt-60 mt-24  lg:h-[700px] h-[350px]"
                 src="/images/phone.png"
                 alt="image"
                 width={300}
-                height={1}
+                height={100}
               />
             </div>
           </div>
